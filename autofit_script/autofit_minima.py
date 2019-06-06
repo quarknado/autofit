fixed bug in covariance matrix building small fixed to make fitting and background subtraction more versatile made jupyter notebook to fit individual states like gf3

autofit_script/autofit_minima.py
@@ -17,6 +17,7 @@ import adminfunctions as ad
 import fittingfunctions as fit
 import autosignals as sig
 
+fstgo = True
 
 '''
 ####################################### Read File and Show Spectrum #############################################
@@ -34,7 +35,7 @@ x, yinit = ad.file_reader(directory + '/' + f)
 spe = ad.spectrum_plotter(x, yinit)
 spe.show()
 
-
+yinit = sig.baseline_subtraction(x,yinit, fstgo)
 
 
 '''
@@ -53,7 +54,7 @@ for x2, y2 in zip(xlist, ylist):
     plt.plot(x2, y2)
 plt.show()
 
-fstgo = True 
+ 
 
 '''
 ##########################################Threshold set and Peak Region Detection###########################################################
@@ -61,7 +62,7 @@ fstgo = True
 rlist, betalist = [],[]
 
 fitlistlist = []
-totalnfits = 30
+totalnfits = 50
 for i in range(totalnfits):  
     while True:
         if fstgo:
@@ -74,7 +75,7 @@ for i in range(totalnfits):
             len_regions = float(input('Approximately how many channels long would you like your fitting segments to be?'))
             no_regions = len(x)/len_regions     
     
-        peak_regions = sig.min_region_finder(xlist, ylist, no_regions)
+        peak_regions = sig.min_region_finder(xlist, ylist, no_regions, fstgo)
         i = 0
         none_indices = []
         for x2, y2 in peak_regions:
@@ -220,6 +221,8 @@ for j, fitlist in enumerate(fitlistlist):
             smfin.append(np.sqrt(ft[2][2*i + 1][2*i + 1]))
             nlist.append(j)
 
+plt.errorbar(nlist, mfin, smfin, linestyle = "")
+plt.show()
 
 
 poslist = []
@@ -234,18 +237,21 @@ for mu, smu, a in zip(mfin, smfin, afin):
         z = np.abs(mu - p)/s
         if np.isnan(p): z = 10000
  
-        if z < 2:
+        if z < 3.5:
             pos.append(mu)
             spos.append(smu)
             h.append(a)
             break
-    if z < 2: continue
+    if z < 3.5: continue
     
     poslist.append([mu])
     sposlist.append([smu])
     alist.append([a])
 
-#print(poslist)
+for i, pos in enumerate(poslist):
+    plt.errorbar(np.full(len(pos), i), pos, sposlist[i], linestyle = "" )
+
+plt.show()
 
 
 
@@ -253,7 +259,7 @@ muarrnew = []
 aarrnew = []
 #print(alist)
 for pos, a in zip(poslist, alist):
-    if len(pos) > 0.8 * totalnfits:
+    if len(pos) > 0.85 * totalnfits:
         muarrnew.append(np.average(pos))
         aarrnew.append(np.average(a))
 
@@ -261,7 +267,7 @@ muarrnew = np.array(muarrnew)
 aarrnew = np.array(aarrnew) 
 
 #print(muarrnew, aarrnew)
-peak_regions = sig.min_region_finder(xlist, ylist, no_regions)
+peak_regions = sig.min_region_finder(xlist, ylist, no_regions, fstgo)
 i = 0
 none_indices = []
 
@@ -338,7 +344,7 @@ for fit in fitlist:
         j = j + 1
     #save peaks to file
     for i, peak in enumerate(fit[3]):
-        line = str(fit[1][2 * i + 1])  + ' ' + str(np.sqrt(fit[2][2 * i + 1][2 * i + 1])) + ' ' + str(peak) + ' ' + str(fit[4][i]) + ' ' + str(fit[1][-3]) + ' ' + str(np.sqrt(fit[2][-5][-5])) + ' ' + str(fit[1][-2]) + ' ' + str(fit[1][-1]) + '\n'
+        line = str(fit[1][2 * i + 1])  + ' ' + str(np.sqrt(fit[2][2 * i + 1][2 * i + 1])) + ' ' + str(peak) + ' ' + str(fit[4][i]) + ' ' + str(fit[1][-3]) + ' ' + str(np.sqrt(fit[2][-3][-3])) + ' ' + str(fit[1][-2]) + ' ' + str(fit[1][-1]) + '\n'
         fil.write(line)
 
 fil.close() 

autofit_script/autosignals.py
@@ -15,11 +15,11 @@ import adminfunctions as ad
 #it's to convolve the whole spectrum with a gaussian equal to the width of the FWHM. It should be shorter than the length of the spectrum, considerably longer than the FWHM
 smoothlength = 200
 
-def baseline_subtraction(x,y):    
+def baseline_subtraction(x,y, fstgo):    
     
     print('The channel with the minimum counts in each segment will be a point on a segmented linearly interpolated background.')
 
-    min_indices, min_x_list, min_y_list, x_split_list, y_split_list = min_spectrum_split(x, y)
+    min_indices, min_x_list, min_y_list, x_split_list, y_split_list = min_spectrum_split(x, y, fstgo)
 
     interp_function = sp.interpolate.interp1d(min_x_list, min_y_list, kind = 'linear', fill_value = 'extrapolate')    
 
@@ -35,8 +35,7 @@ def baseline_subtraction(x,y):
     
     return( y - interp_function(x))
 
-def min_spectrum_split(x, y, base_split = None):
-
+def min_spectrum_split(x, y, fstgo, base_split = None):
     if base_split == None:
         while True:    
             try:        
@@ -56,6 +55,7 @@ def min_spectrum_split(x, y, base_split = None):
         min_index = np.where(y_split_list[i] == min(y_split_list[i]))[0]        
         #sometimes there will be more than one channel with the minimum y. As to not bias this algortithm, I'll pick this randomly.        
         if len(min_index) > 1:
+            if fstgo: print("The number of minima to choose from section ", i, " is:", len(min_index))
             min_index = random.choice(min_index)
         min_index = int(min_index)
         min_x = partx[min_index]
@@ -71,7 +71,7 @@ def min_spectrum_split(x, y, base_split = None):
 
     return(min_indices, min_x_list, min_y_list, x_split_list, y_split_list)
 
-def min_region_finder(xlist, ylist, no_regions):
+def min_region_finder(xlist, ylist, no_regions, fstgo):
     len_xlist = []
     for x in xlist:
         len_xlist.append(len(x))
@@ -83,7 +83,7 @@ def min_region_finder(xlist, ylist, no_regions):
     for x, y in zip(xlist, ylist):
         no_individual_regions = np.around((len(x)/total_length) * no_regions)
         if no_individual_regions != 0:        
-            split = min_spectrum_split(x,y, base_split = no_individual_regions)
+            split = min_spectrum_split(x,y, fstgo, base_split = no_individual_regions)
         else:
             split = [[],[],[],[x],[y]]        
         regions_list_x = split[3]

autofit_script/fittingfunctions.py
@@ -263,44 +263,70 @@ def p1_fixer(p1, rbfix, background, mets):
 
 def cov_fixer(cov, rbfix, background):    
     if rbfix:
-        cov = add2(cov) 
         if not background:
+            scov = cov[-1]
+            scovlong = np.insert(scov, -1, [0.,0.])
+            scovlong = np.expand_dims(scovlong, axis = 1)
+            scovshort = np.delete(scov, -1)
+            scovshort = np.append(scovshort, [0, 0])
+            scovshort = np.expand_dims(scovshort, axis = 0)
+            cov = np.delete(cov,-1, axis = 0)
+            cov = np.delete(cov,-1, axis = 1)
             cov = add2(cov)
+            cov = np.append(cov, scovshort, axis = 0)
+            cov = np.append(cov, scovlong, axis = 1)
+ 
+        cov = add2(cov)
     else:
         if not background:
+            scov = cov[-3]
             rcov = cov[-2]
             bcov = cov[-1]
 
-            rcovlong = np.insert(rcov, -2, [0.,0.])
-            bcovlong = np.insert(bcov, -2, [0.,0.])
+            scovlong = np.insert(scov, -3, [0.,0.])
+            rcovlong = np.insert(rcov, -3, [0.,0.])
+            bcovlong = np.insert(bcov, -3, [0.,0.])
 
+            scovlong = np.expand_dims(scovlong, axis = 1)
             rcovlong = np.expand_dims(rcovlong, axis = 1)
             bcovlong = np.expand_dims(bcovlong, axis = 1)
 
 
+            scovshort = np.delete(scov, -1)
+            scovshort = np.delete(scovshort, -1)
+            scovshort = np.delete(scovshort, -1)
             rcovshort = np.delete(rcov, -1)
+            rcovshort = np.delete(rcovshort, -1)
             rcovshort = np.delete(rcovshort, -1)
             bcovshort = np.delete(bcov, -1)
             bcovshort = np.delete(bcovshort, -1)
+            bcovshort = np.delete(bcovshort, -1)
 
+            scovshort = np.append(scovshort, [0, 0])
             rcovshort = np.append(rcovshort, [0, 0])
             bcovshort = np.append(bcovshort, [0, 0])
-            
+
+            scovshort = np.expand_dims(scovshort, axis = 0)            
             rcovshort = np.expand_dims(rcovshort, axis = 0)
             bcovshort = np.expand_dims(bcovshort, axis = 0)
             
 
             cov = np.delete(cov,-1, axis = 0)
             cov = np.delete(cov,-1, axis = 0)
+            cov = np.delete(cov,-1, axis = 0)
             cov = np.delete(cov,-1, axis = 1)
             cov = np.delete(cov,-1, axis = 1)
+            cov = np.delete(cov,-1, axis = 1)
+
+
             cov = add2(cov)
 
 
+            cov = np.append(cov, scovshort, axis = 0)
             cov = np.append(cov, rcovshort, axis = 0)
-
             cov = np.append(cov, bcovshort, axis = 0)
 
+            cov = np.append(cov, scovlong, axis = 1)
             cov = np.append(cov, rcovlong, axis = 1)
             cov = np.append(cov, bcovlong, axis = 1)
             